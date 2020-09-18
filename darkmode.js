bc188a97bf7c386fb448887b525e37ca71ef13a3
taguaradigital <taguaradigital@hotmail.com>
2020-09-18 14:12:37 -0400

    first commit

---
+++ darkmode.js
@@ -0,0 +1,20 @@
+const darkModeSwitch = document.getElementById("dark-mode-switch");
+let lightMode = (localStorage.getItem("lightMode") === "true") ? true : false
+
+const isDarktMode = () => {
+   document.body.classList.remove('lightmode');
+   localStorage.setItem("lightMode", "false");
+}
+const isLightMode = () => {
+   localStorage.setItem("lightMode", "true");
+   document.body.classList.add('lightmode');
+}
+
+if(lightMode){
+   isLightMode() 
+} 
+
+darkModeSwitch.addEventListener("click", () => {
+   lightMode = !lightMode
+   lightMode ? isLightMode() : isDarktMode();
+})
